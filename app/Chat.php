ac3cfdefd283c169f6efcbec3afbe4798aea3827
AlfredoHAC <alfredohac@gmail.com>
2021-01-14 16:39:08 -0300

    Add: Filled in the Models relationships

--- app/Chat.php
+++ app/Chat.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    //
+    
+    public function users() 
+    {
+        return $this->belongsToMany('App\User');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany('App\Message');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     
+    public function chats()
+    {
+        return $this->belongsToMany('App\Chat');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany('App\Message');
+    }
+
 }
